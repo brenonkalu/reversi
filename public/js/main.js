f573d02c31bc1eaa5c3af5746640441edf3dc5aa
Brenon Kalu <b.kalu.bz@gmail.com>
2017-05-16 23:21:34 -0400

    update to lobby display user name

--- public/js/main.js
+++ public/js/main.js
@@ -6,6 +6,7 @@ function getURLParameters(whichParam)
 {
 	var pageURL = window.location.search.substring(1);
 	var pageURLVariables = pageURL.split('&');
+
 	for(var i = 0; i < pageURLVariables.length; i++) {
 		var parameterName = pageURLVariables[i].split('=');
 		if(parameterName[0] == whichParam) {
@@ -18,6 +19,5 @@ var username = getURLParameters('username');
 if('undefined' == typeof username || !username) {
 	username = 'Anonymous_'+Math.random();
 }
-
 
 $('#messages').append('<h4>'+getURLParameters('username')+'</h4>');
